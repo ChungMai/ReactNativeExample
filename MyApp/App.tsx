f66e41b3f82919fa13096608f72cfeb365dca2c0
Mai Hữu Chung <maihuuchung@gmail.com>
2026-07-21 18:02:28 +0700

    sjmple app.tsx

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,31 +1,49 @@
-import { useState } from 'react';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { HelloWorldScreen } from './HelloWorldScreen';
-import { VegetableListScreen } from './VegetableListScreen';
+// import { useState } from 'react';
+// import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+// import { SafeAreaProvider } from 'react-native-safe-area-context';
+// import { HelloWorldScreen } from './HelloWorldScreen';
+// import { VegetableListScreen } from './VegetableListScreen';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [screen, setScreen] = useState<'home' | 'vegetables'>('home');
+// function App() {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   const [screen, setScreen] = useState<'home' | 'vegetables'>('home');
 
+//   return (
+//     <SafeAreaProvider>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <View style={styles.container}>
+//         {screen === 'home' ? (
+//           <HelloWorldScreen onNavigate={() => setScreen('vegetables')} />
+//         ) : (
+//           <VegetableListScreen onBack={() => setScreen('home')} />
+//         )}
+//       </View>
+//     </SafeAreaProvider>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
+
+// export default App;
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View style={styles.container}>
-        {screen === 'home' ? (
-          <HelloWorldScreen onNavigate={() => setScreen('vegetables')} />
-        ) : (
-          <VegetableListScreen onBack={() => setScreen('home')} />
-        )}
-      </View>
-    </SafeAreaProvider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default App;
